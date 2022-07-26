7324c86e46b02ccbedf2461291abb9beae02ee67
sivacharan582 <82636242+sivacharan582@users.noreply.github.com>
2022-07-26 22:41:02 +0530

    Add files via upload

---
+++ Untitled7.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+1+1
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[2]:
+
+
+fhefj
+
+
+# In[ ]:
+
+
+
+
